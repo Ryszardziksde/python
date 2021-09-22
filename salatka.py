f070b19074aade506f7e6d99a9540b5ce016622a
Ryszardziksde <62832805+Ryszardziksde@users.noreply.github.com>
2021-09-22 12:21:07 +0200

    Add files via upload

--- salatka.py
+++ salatka.py
@@ -1,9 +1,7 @@
 o1 = int(input('Liczba osob uwzgledniona w przepisie '));
-s1 = o1*1;
-s2 = o1*0.5;
-s3 = o1*1;
 o2 = int(input('Ilosc uczestnikow spotkania '));
-print ('Dla ', o2 ,' potrzeba ', s1*o2, 'pomidora', s2*o2, 'ogórka', s3*o2, 'sałaty');
+s1 = o2/o1
+print ('Dla ', o2 ,' osób potrzeba ', s1, 'składników z przepisu');
 
 
 
